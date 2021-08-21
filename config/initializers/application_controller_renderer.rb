72dc790314900ba38ce2e3b65bf888c08ad4b5ca
Amachan-R <amachan1803@outlook.jp>
2021-08-21 12:19:28 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2ac6422eebaecde9f23ffa97dfb58c73aba8e80c81a911265a40cfcf5ce930e550e909e5a7dcc062185af29a1260979b3910fcd4cc70f9b055f96d5bf756490d'
